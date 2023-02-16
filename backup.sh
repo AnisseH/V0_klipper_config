a9c7f52302908f46b6fd832693b5cd3f4a5c47b9
AnisVR6 <anisdu25@gmail.com>
2023-02-16 17:43:41 +0000

    Backup on Thu 16 Feb 2023 05:43:41 PM UTC

---
+++ backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# navigate to the Kipper configuration directory
+cd /home/biqu/printer_data
+
+# add any new or modified files
+git add .
+
+# commit the changes with a timestamp
+git commit -m "Backup on $(date)"
+
+# push the changes to the remote repository
+git push origin master
